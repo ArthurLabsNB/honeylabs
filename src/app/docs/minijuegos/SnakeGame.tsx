0ba221644e6a936789275e3ab392d3349a2e70b1
MANZANITAJUICE <manzanitaindustries@gmail.com>
2025-05-29 23:03:32 -0600

    Secrets

--- src/app/docs/minijuegos/SnakeGame.tsx
+++
@@ -1,7 +0,0 @@
-import SnakeGame from './minijuegos/SnakeGame'
-const MINIJUEGOS: Record<string, React.FC<any>> = {
-  PACMAN: PacmanGame,
-  TETRIS01: TetrisGame,
-  SNAKE: SnakeGame, // Nuevo juego
-  // ...
-}
