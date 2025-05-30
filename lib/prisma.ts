e0034958f6bb09b708d6288baa71009a6cde95ca
MANZANITAJUICE <manzanitaindustries@gmail.com>
2025-05-29 23:28:43 -0600

    Secrets

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,15 @@
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 // Evita crear múltiples clientes Prisma en desarrollo (importante en Next.js)
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  });
+  })
 
-if (process.env.NODE_ENV === 'development') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === 'development') globalForPrisma.prisma = prisma
+
+export default prisma
