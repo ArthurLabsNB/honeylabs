fa3c9813b9b9efa15e8470071a9ccec461d3f2a2
MANZANITAJUICE <manzanitaindustries@gmail.com>
2025-05-29 23:43:31 -0600

    Secrets

--- src/app/api/pacman/score.ts
+++ src/app/api/pacman/score.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import prisma from '@/lib/prisma'
+import { prisma } from '@lib/prisma';
 import { getUsuarioFromSession } from '@/lib/auth' // Ajusta el import según tu autenticación
 
 export async function POST(req: NextRequest) {
