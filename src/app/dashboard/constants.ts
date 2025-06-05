a79b42d47e6d80a83ccd5774976fd8a8e5036a29
ArthurNB <arturonb00@gmail.com>
2025-06-04 21:24:09 -0600

    Test 1

--- src/app/dashboard/constants.ts
+++ src/app/dashboard/constants.ts
@@ -1,4 +1,4 @@
-export const SIDEBAR_GLOBAL_WIDTH = 224;
-export const SIDEBAR_GLOBAL_COLLAPSED_WIDTH = 72;
-export const SIDEBAR_ALMACENES_WIDTH = 192;
-export const NAVBAR_HEIGHT = 70;
+export const SIDEBAR_GLOBAL_WIDTH = 224;            // px, sidebar global expandido
+export const SIDEBAR_GLOBAL_COLLAPSED_WIDTH = 72;   // px, sidebar global colapsado
+export const SIDEBAR_ALMACENES_WIDTH = 192;         // px, sidebar de almacenes
+export const NAVBAR_HEIGHT = 70;                    // px, navbar superior
